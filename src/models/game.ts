62fec2e317ebe79674342951031813c8be23834e
BastianHarttung <info@harttung-fotografie.de>
2021-11-01 11:36:19 +0100

    add stack randomly

--- src/models/game.ts
+++ src/models/game.ts
@@ -3,4 +3,29 @@ export class Game {
   public stack: string[] = [];
   public playedCards: string[] = [];
   public currentPlayer: number = 0;
+
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('clubs_' + i);
+      this.stack.push('diamonds_' + i);
+      this.stack.push('hearts_' + i);
+      this.stack.push('spade_' + i);
+    }
+    shuffle(this.stack);
+  }
+}
+
+function shuffle(stack: string[]) {
+  let currentIndex = stack.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = stack[currentIndex];
+    stack[currentIndex] = stack[randomIndex];
+    stack[randomIndex] = temporaryValue;
+  }
+
+  return stack
 }
