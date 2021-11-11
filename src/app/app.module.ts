0675a87b2c79fadcc657cb161f8966672caf2d6a
BastianHarttung <info@harttung-fotografie.de>
2021-11-11 10:11:13 +0100

    card for description design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from "@angular/material/icon";
 import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
+import { GameDescriptionComponent } from './game-description/game-description.component';
 
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from "@angular/forms";
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +25,8 @@ import {FormsModule} from "@angular/forms";
     StartScreenComponent,
     GameComponent,
     PlayerComponent,
-    DialogAddPlayerComponent
+    DialogAddPlayerComponent,
+    GameDescriptionComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +37,8 @@ import {FormsModule} from "@angular/forms";
     MatDialogModule,
     MatInputModule,
     MatFormFieldModule,
-    FormsModule
+    FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
